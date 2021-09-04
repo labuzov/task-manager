e338d5f9de694b818284c08211f5aca2d4d888fe
Sergey <labuzov2003@gmail.com>
2021-09-05 00:26:34 +0300

    added new folder form validation, changed task styles

--- src/components/App/Content/Sidebar/NewFolderButton/NewFolderButton.jsx
+++ src/components/App/Content/Sidebar/NewFolderButton/NewFolderButton.jsx
@@ -1,3 +1,5 @@
+import { Formik } from 'formik';
+import * as yup from 'yup';
 import { useState } from 'react';
 import getNewId from '../../../../../utils/getNewId';
 import SvgSelector from '../../../../common/SvgSelector/SvgSelector';
@@ -5,45 +7,65 @@ import './NewFolderButton.scss';
 
 
 function NewFolderButton(props) {
-    let [isActive, setIsActive] = useState(false);
-    let [title, setTitle]       = useState('');
-
-    const addNewFolder = () => {
-        props.addNewFolder(getNewId(props.folders), title);
-    }
-
-    const resetLocalState = () => {
-        setIsActive(false);
-        setTitle('');
-    }
+    const [isActive, setIsActive] = useState(false);
     
+
+    const addNewFolder = (folderName) => {
+        props.addNewFolder(getNewId(props.folders), folderName);
+
+        setIsActive(false);
+    }
+
+    const validations = yup.object().shape({
+        folderName: yup.string()
+            .typeError('Must be a string')
+            .required('Name of the folder is required')
+            .max(15, 'Max length is 15 symbols'),
+    })
+
     return (
         <>
             {!isActive &&
-                <button className="new-folder-btn" onClick={ () => { setIsActive(true) }}>
+                <button className="new-folder-btn" onClick={() => { setIsActive(true) }}>
 
                     <div className="new-folder-btn__icon">
                         <SvgSelector id="add" />
-
                     </div>
-                    <div className="new-folder-btn__text">New Folder</div>      
-
-         
-                </button>             
+                    <div className="new-folder-btn__text">New Folder</div>
+                </button>
             }
 
             {isActive &&
-                <div className="new-folder">
-                    <input className="new-folder__input" value={title} 
-                    onChange={ (e) => setTitle(e.currentTarget.value) }
-                    autoFocus />
-                    <div className="new-folder__btns">
-                        <button className="new-folder__cancel-btn btn-type-2" 
-                            onClick={ () => { resetLocalState(); }}>Cancel</button>
-                        <button className="new-folder__add-btn btn-type-1"
-                            onClick={ () => { addNewFolder(); resetLocalState(); }}>Add</button>
+                <Formik
+                    initialValues={{
+                        folderName: '',
+                    }}
+                    validationSchema={validations}
+                    onSubmit={ (values) => addNewFolder(values.folderName) }
+                >
+
+                { ({ values, errors, handleChange, isValid, handleSubmit, dirty }) => (
+                    <div className="new-folder">
+                        <input className={"new-folder__input" + (errors.folderName ? ' error' : '')}
+                            type="text"
+                            name="folderName"
+                            onChange={handleChange}
+                            value={values.folderName}
+                            autoFocus 
+                        />
+                        <div className="new-folder__btns">
+                            <button className="new-folder__cancel-btn btn-type-2"
+                                onClick={() => { setIsActive(false); }}>Cancel</button>
+                            <button 
+                                disabled={!isValid && !dirty}
+                                onClick={handleSubmit}
+                                type="submit"
+                                className="new-folder__add-btn btn-type-1"
+                                >Add</button>
+                        </div>
                     </div>
-                </div>
+                ) }
+                </Formik>
             }
         </>
     )
